2b06a989f8ad2ad557e6074517fe8d3deda323c6
Ido Tanne <idovader@gmail.com>
2021-03-08 15:55:00 -0500

    Task: Adjusted random number by seed generation

--- src/RandomFunctions/randomNumberBySeed.js
+++ src/RandomFunctions/randomNumberBySeed.js
@@ -2,12 +2,12 @@ const seedRandom = require('seedrandom')
 const random = require("random");
 
 class randomNumberBySeed{
-    static randomInt(min, max, seed){
+    randomInt(min, max, seed){
         random.use(seedRandom(seed))
         return random.int(min, max)
     }
 
-    static randomFloat(min, max, seed){
+    randomFloat(min, max, seed){
         random.use(seedRandom(seed))
         return random.float(min, max)
     }
