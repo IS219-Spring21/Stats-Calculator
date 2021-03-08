dd1a4afd37aa35c8bf0db566eb15f47e7d6026f4
Dale <djs93@njit.edu>
2021-03-08 17:17:21 -0500

    Move SRSample to Pop. Samp. folder. Make sure amount cannot be more than list size

--- src/Operations/Statistics/Population Sampling/SimpleRandomSample.js
+++ src/Operations/Statistics/Population Sampling/SimpleRandomSample.js
@@ -1,9 +1,13 @@
+const Rand = require('random')
 module.exports = function SimpleRandomSample(elements, amount){
     let remainingElements = elements;
     let result = [];
+    if(amount>elements.length){
+        amount = elements.length;
+    }
     for (let i = 0; i < amount; i++){
-        let chosenIndex = Math.random() * Math.floor(remainingElements.length);
-        result.push(remainingElements[Math.floor(chosenIndex)])
+        let chosenIndex = Rand.int(0,remainingElements.length);
+        result.push(remainingElements[chosenIndex]);
         remainingElements.splice(chosenIndex, 1);
     }
     return result;
