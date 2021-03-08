27208f5e17bb1f985aea0b6dd6fdec320b53b23e
Dale <djs93@njit.edu>
2021-03-08 15:10:51 -0500

    Calculation now returns same result each time (random functions don't give different results every time you use GetResults)

--- src/Models/Calculation.js
+++ src/Models/Calculation.js
@@ -1,10 +1,12 @@
 module.exports = class Calculation{
+    #result = "";
     constructor(a, b, operation){
         this.a = a;
         this.b = b;
         this.operation = operation;
+        this.#result = this.operation(this.a, this.b);
     }
     GetResults(){
-        return this.operation(this.a,this.b);
+        return this.#result;
     }
 }
