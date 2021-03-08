1cd75e6d65b7b1e335b5fff726f7d09a1ad26ff5
Dale <djs93@njit.edu>
2021-03-08 17:22:34 -0500

    Systematic Sample added

--- src/Operations/Statistics/PopulationSamplingOperations.js
+++ src/Operations/Statistics/PopulationSamplingOperations.js
@@ -1,4 +1,5 @@
 const simpleRandom = require('./Population Sampling/SimpleRandomSample');
+const systematic = require('./Population Sampling/SystematicSample');
 
 class PopulationSamplingOperations{
     static SimpleRandom(a,b){
@@ -9,6 +10,16 @@ class PopulationSamplingOperations{
             throw new Error('a cannot be empty'); // Theoretically, it could be. But the project spec says no.
         }
         return simpleRandom(a,b)
+    }
+
+    static Systematic(a,b){
+        if (!Array.isArray(a)){
+            throw new Error('a must be an Array');
+        }
+        else if (a.length===0){
+            throw new Error('a cannot be empty'); // Theoretically, it could be. But the project spec says no.
+        }
+        return systematic(a,b)
     }
 }
 
